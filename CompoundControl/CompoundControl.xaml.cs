70f327ce0b0500faf590811715c83a6f6c5592da
Chris Pellett <chris.pellett@gmail.com>
2015-01-11 14:32:48 +0000

    Changed to try out a RelativeLayout instead of AbsoluteLayout and placed that in CompoundElement. Hoping this will do a better job of managing child items

--- CompoundControl/CompoundControl.xaml.cs
+++ CompoundControl/CompoundControl.xaml.cs
@@ -2,33 +2,62 @@
 using System.Collections.Generic;
 using Xamarin.Forms;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace CompoundControl
 {
 	public partial class CompoundControl : AbsoluteLayout
 	{
-//		public static readonly BindableProperty ZOrderProperty = BindableProperty.CreateAttached<CompoundControl,int> (
-//			                                                         o => CompoundControl.GetZOrder (o),
-//			                                                         0
-//		                                                         );
-//
 		public CompoundControl ()
 		{
 			InitializeComponent ();
 		}
 
-//		public static int GetZOrder (BindableObject o)
-//		{
-//			return (int)o.GetValue (ZOrderProperty);
-//		}
-
 		protected override void OnChildAdded (Element child)
 		{
 			base.OnChildAdded (child);
-//			var wr = child.GetValue (WidthRequestProperty);
-//			var hr = child.GetValue (HeightRequestProperty);
+
+			var pwr = this.GetValue (WidthRequestProperty);
+			var phr = this.GetValue (HeightRequestProperty);
+			var pb = AbsoluteLayout.GetLayoutBounds (this);
+
+			var wr = (double)child.GetValue (WidthRequestProperty);
+			var hr = (double)child.GetValue (HeightRequestProperty);
+
+
 			AbsoluteLayout.SetLayoutFlags (child, AbsoluteLayoutFlags.PositionProportional);
+//			AbsoluteLayout.SetLayoutFlags (child, AbsoluteLayoutFlags.All);
 			AbsoluteLayout.SetLayoutBounds (child, new Rectangle (0.5, 0.5, -1, -1));
+//			AbsoluteLayout.SetLayoutBounds (child, new Rectangle (0.5, 0.5, wr, hr));
+
+			var f = AbsoluteLayout.GetLayoutFlags (child);
+			var flag = f.HasFlag (AbsoluteLayoutFlags.WidthProportional); // F
+			var flag2 = f.HasFlag (AbsoluteLayoutFlags.HeightProportional); // F
+			var flag3 = f.HasFlag (AbsoluteLayoutFlags.XProportional); // T
+			var flag4 = f.HasFlag (AbsoluteLayoutFlags.YProportional); // T
+			var b = AbsoluteLayout.GetLayoutBounds (child);
+
+			var w = b.Width == AbsoluteLayout.AutoSize; // T
+			var h = b.Height == AbsoluteLayout.AutoSize; // T
+			var sr = ((View)child).GetSizeRequest (320,568); // Box=40x40; Image=100x-1; Label=-1x-1
+		}
+
+		protected override SizeRequest OnSizeRequest (double widthConstraint, double heightConstraint)
+		{
+			var s= base.OnSizeRequest (widthConstraint, heightConstraint);
+			return s;
+		}
+		protected override void LayoutChildren (double x, double y, double width, double height)
+		{
+			var adjustedWidth = this.WidthRequest;
+			var adjustedHeight = this.WidthRequest;
+			base.LayoutChildren (x, y, width, height);
+			foreach (var c in this.Children.Cast<View>()) {
+				var x2 = c.X;
+				var y2 = c.Y;
+				var w2 = c.Width;
+				var h2 = c.Height;
+			}
 		}
 	}
 }
